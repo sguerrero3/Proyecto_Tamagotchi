f80fd4a40ef64a71d119198821cc0a69bb8ead6d
nrincon2302 <n.rincon4@uniandes.edu.co>
2023-11-17 11:07:36 -0500

    Req2

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,130 @@
 #include <Arduino.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <iostream>
 
-#if CONFIG_FREERTOS_UNICORE
-static const BaseType_t app_cpu = 0;
-#else
-static const BaseType_t app_cpu = 1;
-#endif
+// Prototipos de las funciones
+void vStateUpdateTask(void *pvParameters);
 
+// Declaración de variables globales y clases
+class Pet {
+  public:
+    // Variables de estado de la mascota por defecto
+    int hambre = 20;
+    int felicidad = 50;
+    int energia = 100;
+    int suenio = 0;
+    int higiene = 100;
+    int salud = 100;
+    int edad = 0;
+    int peso = 30;
 
-// LED rates
-static const int rate_1 = 500;  // ms
-static const int rate_2 = 323;  // ms
+  // Constructor de la clase
+  Pet() {}
 
-// Pins
-static const int led_pin = LED_BUILTIN;
+  // Destructor de la clase
+  ~Pet() {}
 
-
-// Our task: blink an LED at one rate
-void toggleLED_1(void *parameter) {
-  while(1) {
-    digitalWrite(led_pin, HIGH);
-    vTaskDelay(rate_1 / portTICK_PERIOD_MS);
-    digitalWrite(led_pin, LOW);
-    vTaskDelay(rate_1 / portTICK_PERIOD_MS);
+  // Función para actualizar el estado de la mascota por defecto
+  void updateState() {
+    hambre += 1;
+    felicidad -= 1;
+    energia -= 1;
+    suenio += 1;
+    higiene -= 1;
+    salud -= 1;
+    edad += 1;
+    peso -= 1;
   }
-}
 
-// Our task: blink an LED at another rate
-void toggleLED_2(void *parameter) {
-  while(1) {
-    digitalWrite(led_pin, HIGH);
-    vTaskDelay(rate_2 / portTICK_PERIOD_MS);
-    digitalWrite(led_pin, LOW);
-    vTaskDelay(rate_2 / portTICK_PERIOD_MS);
+  // Funciones para actualizar el estado de la mascota con parámetros
+  void updateHambre(int value) {
+    hambre += value;
   }
+
+  void updateFelicidad(int value) {
+    felicidad += value;
+  }
+
+  void updateEnergia(int value) {
+    energia += value;
+  }
+
+  void updateSuenio(int value) {
+    suenio += value;
+  }
+
+  void updateHigiene(int value) {
+    higiene += value;
+  }
+
+  void updateSalud(int value) {
+    salud += value;
+  }
+
+  void updateEdad(int value) {
+    edad += value;
+  }
+
+  void updatePeso(int value) {
+    peso += value;
+  }
+
+};
+
+// RQ2: Función para actualizar el estado de la mascota
+void vStateUpdateTask(void *pvParameters) {
+    // Declaración del semáforo Mutex para la sincronización de recursos compartidos
+    SemaphoreHandle_t xMutex = xSemaphoreCreateMutex();
+
+    // Obtener la instancia de Pet desde los parámetros
+    Pet* pet = static_cast<Pet*>(pvParameters);
+
+    // Bucle de actualización de estado continuo
+    while (true) {
+        // Pedir recursos compartidos
+        xSemaphoreTake(xMutex, portMAX_DELAY);
+        // Actualizar el estado de la mascota sin intervención del usuario
+        pet->updateState();
+
+        // Imprimir el estado de la mascota en la consola usando Serial.print
+        Serial.print("Estado de la mascota - Hambre: ");
+        Serial.print(pet->hambre);
+        Serial.print(", Felicidad: ");
+        Serial.print(pet->felicidad);
+        Serial.print(", Energia: ");
+        Serial.print(pet->energia);
+        Serial.print(", Sueño: ");
+        Serial.print(pet->suenio);
+        Serial.print(", Higiene: ");
+        Serial.print(pet->higiene);
+        Serial.print(", Salud: ");
+        Serial.print(pet->salud);
+        Serial.print(", Edad: ");
+        Serial.print(pet->edad);
+        Serial.print(", Peso: ");
+        Serial.print(pet->peso);
+        Serial.println();
+                  
+        // Liberar recursos compartidos
+        xSemaphoreGive(xMutex);
+
+        // Dormir durante un tiempo antes de la próxima actualización
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Esperar 1000 milisegundos (1 segundo)
+    }
 }
 
 
-void setup() {
+int main() {
+    // Inicializar una instancia de la clase Pet
+    Pet mascota;
 
-  // Configure pin
-  pinMode(led_pin, OUTPUT);
+    // Crear tarea y pasar la instancia de Pet como parámetro
+    xTaskCreate(vStateUpdateTask, "StateUpdateTask", 1000, &mascota, 1, NULL);
 
-  // Task to run forever
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              toggleLED_1,  // Function to be called
-              "Toggle 1",   // Name of task
-              1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              app_cpu);     // Run on one core for demo purposes (ESP32 only)
+    // Iniciar el programador de tareas de FreeRTOS
+    vTaskStartScheduler();
 
-  // Task to run forever
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              toggleLED_2,  // Function to be called
-              "Toggle 2",   // Name of task
-              1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              app_cpu);     // Run on one core for demo purposes (ESP32 only)
-
-  // If this was vanilla FreeRTOS, you'd want to call vTaskStartScheduler() in
-  // main after setting up your tasks.
-}
-
-
-void loop() {
-  // put your main code here, to run repeatedly:
+    // Nunca deberías llegar aquí si el programador de tareas se inicia correctamente
+    return 0;
 }
