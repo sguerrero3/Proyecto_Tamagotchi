f74e5e063d4b3832c4a1c9ce6df568185305ab44
sguerrero3 <s.guerrero3@uniandes.edu.co>
2023-11-24 11:49:28 -0500

    Funciona menu

--- otros/buzzer.cpp
+++ otros/buzzer.cpp
@@ -1,16 +1,14 @@
 #include <Arduino.h>
+#include <Adafruit_SSD1306.h>
 #define BUZZER_PIN 33
 
 void setup()
 {
   pinMode(BUZZER_PIN, OUTPUT);
+  tone(BUZZER_PIN, 1000);
 }
 
 void loop()
 {
-  tone(BUZZER_PIN, 1000); //Send 1KHz sound signal
-  delay(1000);
 
-  noTone(BUZZER_PIN); //Stop sound
-  delay(1000);
 }
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,53 +1,183 @@
 #include <Arduino.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
 
-#define centerButton 35
-#define leftButton 34
-#define rightButton 32
+#define BUZZER_PIN 33
+#define centerButton 18
+#define leftButton 5
+#define rightButton 19
+#define WIRE Wire
 
-String opcionesMenu[] = {"Menu", "Jugar", "Bañar", "Guardar"};
-int total = 0;
+String opcionesMenu[] = {"< Menu >", "<Casa>","< Jugar >", "<Limpiar>", "<Guardar>"};
+xQueueHandle menuQueue = xQueueCreate(1, sizeof(int));
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &WIRE);
+int estado = 0;
 
-SemaphoreHandle_t xButtonSemaphore;
-
-bool enMenu = false;
-
-void blink() {
-    static unsigned long last_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
+// Detectar boton del centro
+void center_Interrupt()
+{
+  static unsigned long c_last_interrupt_time = 0;
+  unsigned long c_interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
-  if (interrupt_time - last_interrupt_time > 200) 
+  if (c_interrupt_time - c_last_interrupt_time > 200)
   {
-    total += 1;
+    if (estado != 1)
+    {
+      estado = 1;
+      int dataToSend = 1;
+      xQueueSend(menuQueue, &dataToSend, portMAX_DELAY);
+    }
+    else if (estado == 1)
+    {
+      int dataToSend = 4;
+      xQueueSend(menuQueue, &dataToSend, portMAX_DELAY);
+    }
   }
-  last_interrupt_time = interrupt_time;
+  c_last_interrupt_time = c_interrupt_time;
+}
+
+// Detectar boton de la izquierda
+void left_Interrupt()
+{
+  static unsigned long l_last_interrupt_time = 0;
+  unsigned long l_interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (l_interrupt_time - l_last_interrupt_time > 200)
+  {
+    if (estado == 1)
+    {
+      int dataToSend = 2;
+      xQueueSend(menuQueue, &dataToSend, portMAX_DELAY);
+    }
+  }
+  l_last_interrupt_time = l_interrupt_time;
+}
+
+// Detectar boton derecha
+void right_Interrupt()
+{
+  static unsigned long r_last_interrupt_time = 0;
+  unsigned long r_interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (r_interrupt_time - r_last_interrupt_time > 200)
+  {
+    if (estado == 1)
+    {
+      int dataToSend = 3;
+      xQueueSend(menuQueue, &dataToSend, portMAX_DELAY);
+    }
+  }
+  r_last_interrupt_time = r_interrupt_time;
+}
+
+// Task para el menu
+void vMenuTask(void *parameter)
+{
+  int receivedData = 0;
+  int indexMenu = 0;
+
+  while (1)
+  {
+
+    if (xQueueReceive(menuQueue, &receivedData, portMAX_DELAY))
+    {
+
+      if (receivedData == 1)
+      {
+        indexMenu = 0;
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.print(opcionesMenu[indexMenu]);
+        display.display();
+        receivedData = 0;
+      }
+      else if (receivedData == 2)
+      {
+        if (indexMenu == 0){indexMenu = 3;}
+        else{indexMenu -= 1;}
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.print(opcionesMenu[indexMenu % 4]);
+        display.display();
+        receivedData = 0;
+      }
+      else if (receivedData == 3)
+      {
+        if (indexMenu == 3){indexMenu = 0;}
+        else{indexMenu += 1;}
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.print(opcionesMenu[indexMenu % 4]);
+        display.display();
+        receivedData = 0;
+      }
+      else if (receivedData == 4)
+      {
+        Serial.print(indexMenu);
+        if(indexMenu == 1){
+          display.clearDisplay();
+          display.setCursor(0, 0);
+          display.print("Task Casa");
+          display.display();
+          estado = 2;
+          receivedData = 0;
+        }
+        else if(indexMenu == 2){
+          display.clearDisplay();
+          display.setCursor(0, 0);
+          display.print("Task Jugar");
+          display.display();
+          estado = 3;
+          receivedData = 0;
+        }
+        else if(indexMenu == 3){
+          display.clearDisplay();
+          display.setCursor(0, 0);
+          display.print("Task Limpiar");
+          display.display();
+          estado = 4;
+          receivedData = 0;
+        }
+      }
+    }
+  }
 }
 
 
+void setup()
+{
+  pinMode(BUZZER_PIN, OUTPUT);
 
-void setup() {
+  // Inicializar pantalla
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
+  delay(500);
+  display.setRotation(2);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("Este NO es el menu");
+  display.display();
 
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    xButtonSemaphore = xSemaphoreCreateBinary();
+  // Pines para botones
+  pinMode(centerButton, INPUT_PULLUP);
+  pinMode(leftButton, INPUT_PULLUP);
+  pinMode(rightButton, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(centerButton), center_Interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(leftButton), left_Interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(rightButton), right_Interrupt, FALLING);
 
-    if (xButtonSemaphore == NULL) {
-        Serial.println("Semaphore creation failed!");
-        while (1);
-    }
-    else{
-        Serial.println("Semaphore creation Good!");
-    }
-
-    pinMode(centerButton, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(centerButton), blink, CHANGE);
-
+  // Tarea para menu
+  xTaskCreate(vMenuTask, "MenuTask", 2048, NULL, 1, NULL);
 }
 
-
-void loop() {
-  // put your main code here, to run repeatedly:
-    Serial.println(total);
-    delay(500);
+void loop()
+{
+  delay(500);
 }
